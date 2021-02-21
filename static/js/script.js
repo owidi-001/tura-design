187e1347239e75e155b6eb8c06402701b8313232
whoami <whoami@localhost.localdomain>
2021-02-21 14:18:38 -0500

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+
+//   var instance = M.Carousel.getInstance(elem);
+});
+
+// Or with jQuery
+
+// $(document).ready(function () {
+//     $('.carousel').carousel();
+// });
